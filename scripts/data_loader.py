1b5a530a29d385036d52be587a5f59fe0a549064
Cristian Riofrio <cristianriofrio@hotmail.com>
2025-10-18 21:00:39 -0500

    Listo

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import os
 
-# Ruta absoluta de la carpeta dond e se encuentra este script (...../scripts/data_loader.py
+# Ruta absoluta de la carpeta donde se encuentra este script (.../scripts/data_loader.py)
 SCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Ruta al archivo CSV dentro de la carpeta data
 DATA_PATH = os.path.join(SCRIPTS_DIR, "..", "data", "games.csv")
 
 def cargar_datos(path):
     print(f"Cargando datos desde: {path}...")
-    
     try:
         df = pd.read_csv(path)
         print("Datos cargados exitosamente.")
@@ -19,4 +19,20 @@ def cargar_datos(path):
     except Exception as e:
         print(f"Ocurrió un error al cargar los datos: {e}")
         return None
-    
+
+if __name__ == "__main__":
+    print(f"Iniciando el proceso de carga de datos... : {os.path.abspath(__file__)}")
+
+    # Llamamos a la función para cargar los datos
+    dataframe_juegos = cargar_datos(DATA_PATH)
+
+    if dataframe_juegos is not None:
+        print("\n--Primeras 5 filas del DataFrame--")
+        print(dataframe_juegos.head())
+
+        print("\n--Información del DataFrame--")
+        print(dataframe_juegos.info())
+
+         
+         
+         
